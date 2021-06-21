daca1b0955ef1b971d542c30eec7693b8faf75a3
Rrch <rrch.cr8@gmail.com>
2021-06-20 23:14:18 -0400

    added UnitTests

---
+++ Counting Duplicates/TestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Counting_Duplicates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, Kata.DuplicateCount(""));
+            Assert.AreEqual(0, Kata.DuplicateCount("abcde"));
+            Assert.AreEqual(2, Kata.DuplicateCount("aabbcde"));
+            Assert.AreEqual(2, Kata.DuplicateCount("aabBcde"), "should ignore case");
+            Assert.AreEqual(1, Kata.DuplicateCount("Indivisibility"));
+            Assert.AreEqual(2, Kata.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+        }
+    }
+}
