47d401ced917ab89141fdd9b4d8dfe89e85ece2d
Rrch <rrch.cr8@gmail.com>
2021-06-21 11:47:31 -0400

    project created
    Signed-off-by: Rrch <rrch.cr8@gmail.com>

---
+++ StringToFraction/StringToFraction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringToFraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
