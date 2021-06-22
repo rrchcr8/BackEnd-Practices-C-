7df8f22d016146570cf7ac6f370248488ee44b45
Rrch <rrch.cr8@gmail.com>
2021-06-22 17:59:00 -0400

    initial commit

---
+++ NullableExercise/NullableExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NullableExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> nx = new Nullable<int>();
+            //int? nx = null;
+            int x=6;
+            
+            
+            Console.WriteLine("nx has a value? {0}", nx.HasValue);
+            Console.WriteLine("x == nx: {0}", x == nx);
+            Console.WriteLine("x != nx: {0}", x != nx);
+
+
+            nx = 20;
+            x = 5;
+
+            Console.WriteLine("nx has a value? {0}", nx.HasValue);
+            Console.WriteLine("nx has a value? {0}", nx.Value);
+            Console.WriteLine("x == nx: {0}", x == nx);
+            Console.WriteLine("x != nx: {0}", x != nx);
+
+
+
+            //Console.WriteLine("nx has a value? {0}", nx.HasValue);
+            //Console.WriteLine("nx has a value? {0}", nx.HasValue);
+            //Console.WriteLine("nx has a value? {0}", nx.HasValue);
+            //Console.WriteLine("nx has a value? {0}", nx.HasValue);
+            //Console.WriteLine("nx has a value? {0}", nx.HasValue);
+            //Console.WriteLine("nx has a value? {0}", nx.HasValue);
+
+
+
+        }
+    }
+}
