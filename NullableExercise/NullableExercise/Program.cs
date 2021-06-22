5b6271497884a01cb7191f4919bf9ea328292efa
Rrch <rrch.cr8@gmail.com>
2021-06-22 18:03:32 -0400

    completed

--- NullableExercise/NullableExercise/Program.cs
+++ NullableExercise/NullableExercise/Program.cs
@@ -28,9 +28,14 @@ namespace NullableExercise
             Console.WriteLine("nx = {0}", nx ?? -1);
             Console.WriteLine("nx = {0}", nx.GetValueOrDefault());
             Console.WriteLine("nx = {0}", nx.GetValueOrDefault(-2));
-            
-            
-            
+
+            nx = 10;
+            Console.WriteLine("nx = {0}", nx ?? -1);
+            Console.WriteLine("nx = {0}", nx.GetValueOrDefault());
+            Console.WriteLine("nx = {0}", nx.GetValueOrDefault(-2));
+
+
+
 
 
             //Console.WriteLine("nx has a value? {0}", nx.HasValue);
