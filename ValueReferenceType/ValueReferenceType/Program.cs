6175a24e7cd2181275d7e1dcb6b12024eadf2e85
Rrch <rrch.cr8@gmail.com>
2021-06-24 01:41:41 -0400

    progress

--- ValueReferenceType/ValueReferenceType/Program.cs
+++ ValueReferenceType/ValueReferenceType/Program.cs
@@ -5,18 +5,23 @@ namespace ValueReferenceType
 {
     class Program
     {
+     
         static void Main(string[] args)
         {
 
             var n1 = new DetailedInteger(0);
             n1.AddDetail("A");
+            //Console.WriteLine(nameof(n1)); // n1
             Console.WriteLine(n1); //0 [A]
 
             var n2 = n1;
             n2.Number = 7;
+            
             n2.AddDetail("B");
 
+            //Console.WriteLine(nameof(n1)); //n1
             Console.WriteLine(n1); //0 [A, B]
+            //Console.WriteLine(nameof(n2)); //n2 
             Console.WriteLine(n2); //7 [A, B] 
 
         }
@@ -24,13 +29,17 @@ namespace ValueReferenceType
 
     class DetailedInteger
     {
+
+        public DetailedInteger d;
         public int Number;
         List<string> Array = new List<string>();
         
 
         public DetailedInteger(int Number)
         {
-            this.Number = Number;
+            this.d.Number = Number;
+                       
+            
         }
 
         internal void AddDetail(string v)
@@ -39,9 +48,12 @@ namespace ValueReferenceType
         }
 
         public override string ToString() {
-            string n3 = Number.ToString();
-            return n3+" ["+String.Join(", ", Array) + "]";
-
+            //return Number.GetHashCode() + " [" + String.Join(", ", Array) + "]"+"gato";
+            if (nameof(d).Equals("n1"))
+            {
+                return d.Number + " [" + String.Join(", ", Array) + "]";
+            }
+            else { return Number + " [" + String.Join(", ", Array) + "]"; }
         }
 
 
