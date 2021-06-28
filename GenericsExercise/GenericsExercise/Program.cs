a8f5ecbe36a5a393faec5711c97ca744e42fbc52
Rrch <rrch.cr8@gmail.com>
2021-06-28 09:51:16 -0400

    // where T : struct

--- GenericsExercise/GenericsExercise/Program.cs
+++ GenericsExercise/GenericsExercise/Program.cs
@@ -4,7 +4,8 @@ namespace GenericsExercise
 {
     class Program
     {
-        public static void Swap<T>(ref T a, ref T b) {
+        public static void Swap<T>(ref T a, ref T b)
+        {
             T c = a;
             a = b;
             b = c;
@@ -29,22 +30,41 @@ namespace GenericsExercise
             Console.WriteLine("a = {0}, b = {1}", a, b);
             Console.WriteLine("c = {0}, c = {1}", c, d);
 
+            // where T : new()	The type argument must have a public parameterless constructor
+
             MyGenerics<PrivateClass> pi = new();
             MyGenerics<string> sss = new();
             MyGenerics<PublicClass> p2 = new();
+
+            // where T : struct
+
+            // all this tipe are non nullaBLE TYPES
+            GenericsStructRestrains<StrucClass> qw = new();
+            GenericsStructRestrains<int> qw2 = new();
+            GenericsStructRestrains<double> qw1233 = new();
+
+            //must be a non nullable type
+            GenericsStructRestrains<String> qw2 = new();
+            GenericsStructRestrains<PublicClass> qw3 = new();
+
+
+
         }
 
 
+        // where T : new()
         public class MyGenerics<T> where T : new()
         {
         }
 
-        public class PrivateClass{
+        public class PrivateClass
+        {
 
-            private PrivateClass() { 
+            private PrivateClass()
+            {
             }
 
-            }
+        }
     }
 
     public class PublicClass
@@ -55,5 +75,14 @@ namespace GenericsExercise
         }
 
     }
+
+    public class GenericsStructRestrains<T> where T : struct { 
+    }
+
+    struct StrucClass { 
+    
+    }
+
+
 }
-}
+
