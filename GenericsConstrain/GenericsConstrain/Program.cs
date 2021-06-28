2d80e4e0b0f9cb00745b25d1208030a96ba9b4e8
Rrch <rrch.cr8@gmail.com>
2021-06-28 16:40:38 -0400

    final solution

--- GenericsConstrain/GenericsConstrain/Program.cs
+++ GenericsConstrain/GenericsConstrain/Program.cs
@@ -7,13 +7,21 @@ namespace GenericsConstrain
         static void Main(string[] args)
         {
             Person.Unique.name = "pepe";
+            Console.WriteLine(Person.Unique);
+            Console.WriteLine(Person.Unique.name);
+
+            Person.Unique.name = "pedro";
+            Console.WriteLine(Person.Unique);
             Console.WriteLine(Person.Unique.name);
         }
     }
     public class UniquePerson<T> where T: new()
     {
-        public static T Unique = new();
-        
+        // public static T Unique = new();
+        private static T UniquePrivate;
+        public static T Unique  { get => UniquePrivate == null ? UniquePrivate = new() : Unique; }
+
+       
     }
 
     public class Person : UniquePerson<Person>
