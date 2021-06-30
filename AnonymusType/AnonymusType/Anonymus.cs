f057e83c04664c135fc6aeb2949ddbcbf71e25a7
Rrch <rrch.cr8@gmail.com>
2021-06-30 09:22:44 -0400

    solution anonymus tyoe exercise

---
+++ AnonymusType/AnonymusType/Anonymus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymusType
+{
+    class Anonymus
+    {
+        public object getData()
+        {
+            return new { Name = "Pepe", EmailID = "pepe@gmail.com" };
+        }
+    }
+
+}
---
+++ AnonymusType/AnonymusType/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AnonymusType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Anonymus anonymus = new Anonymus();
+            dynamic anonymusDynamicData = anonymus.getData();
+            Console.WriteLine(String.Format("{0} {1}", anonymusDynamicData.Name, anonymusDynamicData.EmailID));
+
+            object anonymusData = anonymus.getData();
+            var obj = new { Name = "", EmailID =""};
+            obj = Cast(obj, anonymusData);
+            Console.WriteLine(String.Format("{0} {1}", obj.Name, obj.EmailID));
+            Console.WriteLine(String.Format("{0} {1}", anonymusData.Name, anonymus.EmailID));
+
+
+            var v = new { Amount = 108, Message = "Hello" };
+
+            // Rest the mouse pointer over v.Amount and v.Message in the following
+            // statement to verify that their inferred types are int and string.
+            Console.WriteLine(v.Amount + v.Message);
+
+        }
+
+        //public object getData() {
+        //    return new { Name = "Pepe", EmailID = "dsfsd@gato.com" };
+        //}
+
+        private static T Cast<T>(T typeHolder, Object x) 
+        {
+            return (T)x;
+        }
+    
+    }
+}
