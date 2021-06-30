b8f775d188d61f565618cd54209bce9b6d837b34
Rrch <rrch.cr8@gmail.com>
2021-06-30 15:27:34 -0400

    Icomparer implementation

--- InterfaceExercise/InterfaceExercise/Program.cs
+++ InterfaceExercise/InterfaceExercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InterfaceExercise
 {
@@ -15,13 +16,15 @@ namespace InterfaceExercise
             new Person {Name = "Richard" }
             };
 
-            Console.WriteLine( "Initial List of peole:");
-            foreach (var person in people) {
+            Console.WriteLine("Initial List of peole:");
+            foreach (var person in people)
+            {
 
                 Console.WriteLine($"{person.Name}");
             }
             Console.WriteLine("Use Person's Icomparable implementation to sort");
-            Array.Sort(people);
+            //Array.Sort(people);
+            Array.Sort(people, new PersonComparer());
             foreach (var person in people)
             {
                 Console.WriteLine($"{person.Name}");
@@ -30,13 +33,30 @@ namespace InterfaceExercise
         }
     }
 
-   class Person:IComparable<Person>
+    //class Person : IComparable<Person>
+    //{
+    //    public string Name { get; set; }
+
+    //    public int CompareTo(Person obj)
+    //    {
+    //        return Name.CompareTo(obj.Name);
+    //    }
+    //}
+
+
+    class Person 
     {
         public string Name { get; set; }
 
-        public int CompareTo(Person obj)
-        {
-            return Name.CompareTo(obj.Name);
-                }
     }
+
+    class PersonComparer : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return x.Name.CompareTo(y.Name);           
+        
+        }
+    }
+
 }
