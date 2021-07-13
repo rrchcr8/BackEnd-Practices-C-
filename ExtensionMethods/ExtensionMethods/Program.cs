cbf66c6b8bcc7a9276c2a6d77bddbc04f704117a
Rrch <rrch.cr8@gmail.com>
2021-07-13 10:50:59 -0400

    extension methods example 1

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        /*
+         * Its a way to add methods to an existing type.
+         */
+        static void Main(string[] args)
+        {
+            //Extensions.FizzBuzz(2);
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(i.FizzBuzz());
+            }
+
+        }
+
+
+    }
+
+    static class Extensions
+    {
+        /*
+         * the first paramaeter shuld inlude this
+         */
+        public static string FizzBuzz(this int value)
+        {
+            if (value % 3 == 0 && value % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (value % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (value % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+    }
+}
