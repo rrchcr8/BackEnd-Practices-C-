56a2eb0cbf8abdf8765903a39d2838cbaa5b7c59
Rrch <rrch.cr8@gmail.com>
2021-07-16 15:59:45 -0400

    inital commit

---
+++ ExeptionExecise/ExeptionExecise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+
+namespace ExeptionExecise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient webClient = null;
+
+            try
+            {
+                webClient = new WebClient();
+                var result = webClient.DownloadString("https://www.google.com");
+            }
+            catch (ArgumentNullException ex)
+            {
+
+            }
+            catch (WebException ex)
+            w//hen ((ex.Response as HttpWebResponse)?.StatusCode == HttpStatusCode.NotFound)
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+            finally {
+                webClient.Dispose();
+            }
+        }
+    }
+}
