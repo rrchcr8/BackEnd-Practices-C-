4903e90e29f3d846467aba0eab477261dcb9de6e
Rrch <rrch.cr8@gmail.com>
2021-07-30 13:33:39 -0400

    more tests added

--- OutlookClient/OutlookClient/Program.cs
+++ OutlookClient/OutlookClient/Program.cs
@@ -24,9 +24,27 @@ namespace OutlookClient
             OutlookClient CarlosOutlookClient = new OutlookClient(CarlosAccount,server);
             OutlookClient LauraOutlookClient = new OutlookClient(LauraAccount, server);
 
+            Console.WriteLine("initial count of items in inbox");
+            Console.WriteLine(LauraOutlookClient.folders[0].FolderName);
+            Console.WriteLine(LauraOutlookClient.folders[0].Content.Count);
+
+            //crichter creates a messagge
             Message message = CarlosOutlookClient.CreateMessage("lrod@jala.com","content test", "PRUEBA ONE");
             CarlosOutlookClient.send(message);
 
+            Console.WriteLine("after receiving an email");
+            Console.WriteLine(LauraOutlookClient.folders[0].FolderName);
+            Console.WriteLine(LauraOutlookClient.folders[0].Content.Count);
+
+
+            Console.WriteLine("Now we will try to send a message that contains CF Bolivar");
+            Message messageNasty = CarlosOutlookClient.CreateMessage("lrod@jala.com", "this is a body that contains the forbidden word CF Bolivar", "PRUEBA polity");
+            CarlosOutlookClient.send(messageNasty);
+
+            Console.WriteLine("after receiving the second email");
+            Console.WriteLine("folder name: {0} ", LauraOutlookClient.folders[0].FolderName);
+            Console.WriteLine("Count:  {0}", LauraOutlookClient.folders[0].Content.Count);
+            Console.WriteLine("mail  {0}", LauraOutlookClient.folders[0].Content[1]);
 
 
             //creates a folder
