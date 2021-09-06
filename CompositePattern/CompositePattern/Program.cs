f0e183707743719f09884204bc28d2221ae675fa
carlos richter <rrch.cr8@gmail.com>
2021-09-06 14:38:14 -0400

    program code

--- CompositePattern/CompositePattern/Program.cs
+++ CompositePattern/CompositePattern/Program.cs
@@ -13,6 +13,17 @@ namespace CompositePattern
             p1.setStartTag("<body>");
             p1.setEndTag("</body>");
             parentTag.addChildtag(p1);
+            HtmlTag child1 = new HtmlElement("<p>");
+            child1.setStartTag("<p>");
+            child1.setEndTag("</p>");
+            child1.setTagBody("testing html tag library");
+            p1.addChildtag(child1);
+            HtmlTag child2 = new HtmlElement("<p>");
+            child2.setStartTag("<p>");
+            child2.setEndTag("</p>");
+            child2.setTagBody("Paragraph 2");
+            p1.addChildtag(child2);
+            parentTag.generateHtml();
 
         }
     }
