363e509419c6df326211d132999eebbc576a05da
carlos richter <rrch.cr8@gmail.com>
2021-09-26 20:30:12 -0400

    initial commit

---
+++ RAbbitMQDemo/RAbbitMQDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using RabbitMQ;
+using RabbitMQ.Client;
+
+namespace RAbbitMQDemo
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhot:5672")
+
+            };
+        }
+    }
+     
+}
